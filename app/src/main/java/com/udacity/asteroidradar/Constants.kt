c9a309a912c586cd0266e50eeaa33fb1256e469d
testone <@>
2023-01-20 13:00:57 +0200

    Enter_api

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "epW4zdy13t0SlJb2eJtBa9breCNLJenx6eakth2d"
+    const val API_KEY = "Enter_API_KEY"
 }
